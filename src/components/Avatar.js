84c3319c183a23e03a18043e16cca63e6ae24c5c
cypressg <krasauskas.kipras@gmail.com>
2021-12-21 20:42:28 +0200

    Added Button component

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,10 +1,10 @@
 import profile from "../images/profile.jpeg";
-
+import Button from "./Button";
 const Avatar = () => {
   return (
     <div className="avatar-container">
       <img src={profile} alt="avatar"></img>
-      
+      <Button />
     </div>
   );
 };
